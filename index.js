faca99e3073a07bc8f4560a55f0ab3c09098ef9f
mohammad syed hasan <syedhasanmohammad@gmail.com>
2024-04-29 09:56:37 +0600

    getting single data from db for View Details page

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const cors = require("cors");
 const port = process.env.PORT || 5000;
 const users = [
@@ -44,6 +44,13 @@ async function run() {
     app.get("/craftsitems", async (req, res) => {
       const cursor = craftsCollection.find();
       const result = await cursor.toArray();
+      res.send(result);
+    });
+    // getting single data for viewDetails Page
+    app.get("/craftsitems/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await craftsCollection.findOne(query);
       res.send(result);
     });
 
