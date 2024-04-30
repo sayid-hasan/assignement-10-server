30b4f671c6e705816287e13de6f5bb014cd85fe1
mohammad syed hasan <syedhasanmohammad@gmail.com>
2024-04-30 21:32:30 +0600

    solved deploy related bugs

--- index.js
+++ index.js
@@ -23,10 +23,10 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+    // // Send a ping to confirm a successful connection
+    // await client.db("admin").command({ ping: 1 });
 
     const craftsCollection = client.db("CraftsDB").collection("crafts");
     const categoriesItemsCollection = client
